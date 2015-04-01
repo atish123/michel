6a4c8da3a20e34e33a8bbbe521505f0b20617c94
atish <aatish93maske@gmail.com>
2015-04-01 15:14:10 +0530

    init

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.16'
+
+
+gem 'sqlite3'
+gem 'execjs'
+gem 'therubyracer'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :production do
+  gem 'pg',             '0.17.1'
+  gem 'rails_12factor', '0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Michel::Application.config.secret_token = '3781780323c482b623f902d171ffc707d11a7390a9a5189b5fec4d2960abb60c1c0d45804c3bfc7b9f24dc1e8e2d5a35ea9ebdc2d294297d9bd0e93d97e4a38f'
